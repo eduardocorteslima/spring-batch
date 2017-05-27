e145fb846cf87512e9b76128221d91a86adaf4b1
eduardocortes <eduardo.lima@fcamara.com.br>
2017-05-27 20:56:31 -0300

    First

---
+++ src/main/java/batch/integration/file/Application.java
@@ -0,0 +1,12 @@
+package batch.integration.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
